dcceffc3a88df6b0710dd506f890a1e5a9dbbaa9
Michel <140141004+CrisMichel@users.noreply.github.com>
2025-05-05 06:56:46 -0600

    Switch xml

--- modules/extractor.py
+++ modules/extractor.py
@@ -1,27 +1,17 @@
+from lxml import html
 import requests
-from bs4 import BeautifulSoup
 
 def extract_url_data(url):
-    """
-    Extrae contenido de una página web usando requests + BeautifulSoup.
-    """
-
     try:
         response = requests.get(url)
         response.raise_for_status()
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        tree = html.fromstring(response.content)
 
-        # Titulo
-        title = soup.title.string if soup.title else "Dato no disponible"
+        title = tree.findtext('.//title') or "Dato no disponible"
 
-        # Meta descripción
-        description_tag = soup.find("meta", attrs={"name": "description"})
-        description = description_tag["content"] if description_tag else "Dato no disponible"
-
-        # Texto del cuerpo
-        paragraphs = soup.find_all('p')
-        text = "\n".join([p.get_text() for p in paragraphs])
+        paragraphs = tree.xpath('//p/text()')
+        text = "\n".join(paragraphs)
 
         return {
             "title": title.strip(),
